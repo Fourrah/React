22e4289cf7c22f83dc4ba9934688b51fcecef6ea
Sara_Jeon <saraj@tecace.com>
2023-05-16 16:24:09 +0900

    ES^ array methods

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFristElement = <h1>Hello React!</h1>
+const myArray = ['apple', 'banana', 'orange'];
+
+const myList = myArray.map((item) => <p>{item}</p>)
+
+
+// const myFristElement = <h1>Hello React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFristElement);
+root.render(myList);
