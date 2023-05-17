38935a9c8b13b4af789390fbacdc790f37b5e5d7
Sara_Jeon <saraj@tecace.com>
2023-05-17 11:21:25 +0900

    React class

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -43,6 +43,97 @@ function Garage() {
   )
 }
 
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  shouldComponentUpdate() {
+    return true; //true일 때만 렌더링 됨!
+  }
+  changeColor = () => {
+    this.setState({favoritecolor: "blue"});
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }
+}
+
+class Header2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+
+  //마운트 된 직후에 호출
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({favoritecolor: "yellow"})
+    }, 1000)
+  }
+  
+  //업데이트 되기 전 값을 알려줌
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML =
+    "Before the update, the favorite was " + prevState.favoritecolor;
+  }
+
+  //업데이트 된 후의 값도 알려줌
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML =
+    "The updated favorite is " + this.state.favoritecolor;
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <div id="div1"></div>
+      <div id="div2"></div>
+      </div>
+    );
+  }
+}
+
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {show: true};
+  }
+  delHeader = () => {
+    this.setState({show: false});
+  }
+  render() {
+    let myheader;
+    if (this.state.show) {
+      myheader = <Child />;
+    };
+    return (
+      <div>
+      {myheader}
+      <button type="button" onClick={this.delHeader}>Delete Header</button>
+      </div>
+    );
+  }
+}
+
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("The component named Header is about to be unmounted.");
+  }
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myList);
 root.render(myElement);
@@ -54,3 +145,6 @@ root.render(myElement6);
 root.render(myElement7);
 root.render(<Garage />);
 root.render(<Car2 />);
+root.render(<Header />);
+root.render(<Header2 />);
+root.render(<Container />);
