2e92baeba6f65363ca84743917ce3da3b13e9c27
Sara_Jeon <saraj@tecace.com>
2023-06-08 17:13:49 +0900

    Start Movie App Project

---
+++ react-for-beginners-2/src/App4.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App4() {
+  return <Router>
+    <Routes>
+      <Route path="/movie" element={<Detail />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>
+}
+
+export default App4;
